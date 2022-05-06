d3a806e3cbf30a6461359fe21e79bf1ba1a9e3af
Kaemerle <carlkaemerle@yahoo.com>
2022-05-06 16:12:07 -0700

    starting from scratch

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
 'use strict';
 
-const divContainer = document.querySelector('.divContainer');
-
-function addRow() {
-    const row = document.createElement('div');
-    divContainer.appendChild(row);
-    for(let i = 0; i < 16; i++) { 
-        const column = document.createElement('div'); 
-        column.textContent = 'column'
-        column.style.background = 'lightblue';
-        column.style.border = 'solid 1px black';
-        column.style.marginTop = '1px';
-        row.appendChild(column);
-
-    }
-};
-
-function creatGrid() {
-    for(let i = 0; i < 16; i++) {
-        addRow();
-    }
-}
-creatGrid();

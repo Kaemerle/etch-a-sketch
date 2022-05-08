6bd73322f926a9d76b85f41289ba2ec500ebdf87
Kaemerle <carlkaemerle@yahoo.com>
2022-05-07 20:24:20 -0700

    resetting again

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-'use strict';
-
-const body = document.querySelector('body');
-const container = document.querySelector('#container')
-const box = document.querySelector('.box');
-const header = document.createElement('div');
-header.classList.add('header');
-header.textContent = 'Etch-A-Sketch'
-body.insertBefore(header, container)
-const grid = document.createElement('div');
-grid.classList.add('grid');
-box.appendChild(grid);
-grid.textContent = 'grid';
+'use-strict';

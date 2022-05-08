50a830b31446912357363fef094840ebf016497a
Kaemerle <carlkaemerle@yahoo.com>
2022-05-07 21:26:12 -0700

    finally made a clean html and a proper grid function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use-strict';
+// variables
+
+const container = document.querySelector('#grid-container');
+
+// functions
+
+function createGrid(size) {
+    for(let i = 0; i < size; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for(let j = 0; j < size; j++) {
+            let column = document.createElement('div');
+            column.classList.add('column');
+            row.appendChild(column);
+        };
+    };
+};
+
+createGrid(16)

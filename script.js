d1cebb357e461510c32e29dfc3df6df44fb073c7
Kaemerle <84556491+Kaemerle@users.noreply.github.com>
2022-05-14 14:01:12 -0700

    Update script.js
    commenting out script

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const lrgBtn = document.querySelector('#lrgBtn');
 
 // functions ------------------
 
-
+        // runs two different for loops, 1 for rows and 1 for columns. 
+        // only adds a left and top border to prevent double pixelated borders
 function createGrid(size) {
     for(let i = 0; i < size; i++) {
         let row = document.createElement('div');
@@ -36,7 +37,7 @@ function removeChildNodes () {
 };
 
 container.addEventListener('mouseover', function (e) {
-    // Add the "active" class to only divs with a "box" class
+    // Add the "draw" class to only divs with a "column" class
     if (e.target.matches('.column')) {
       e.target.classList.add('draw');
     }
@@ -45,6 +46,7 @@ container.addEventListener('mouseover', function (e) {
 // default grid size
 createGrid(16)
 
+// buttons to determine grid size
 reset.addEventListener('click', () => {
 removeChildNodes()
 createGrid(0);

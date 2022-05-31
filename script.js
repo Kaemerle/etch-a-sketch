12018206f2afb5412b17209b972781bed3834709
Kaemerle <carlkaemerle@yahoo.com>
2022-05-31 10:28:06 -0700

    done with a more simple version of coloring cells

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function createGrid(num) {
     let row = document.createElement("div");
     row.classList.add("row");
     divContainer.appendChild(row);
+    row.style.backgroundColor = " white";
     for (let j = 0; j < num; j++) {
       let column = document.createElement("div");
       column.classList.add("column");
@@ -23,7 +24,41 @@ function createGrid(num) {
       column.style.borderTop = "1px black solid";
       // only adds a top and left border to prevent borders from stacking
       row.appendChild(column);
+      column.addEventListener("mouseover", () => {
+        column.style.backgroundColor = "black";
+      });
     }
   }
 }
-createGrid(16);
+
+//  default grid size
+createGrid(24);
+
+//  a function to clear the grid to use as a reset button and to use at the beggining of other grid resizing functions
+
+function removeChildNodes() {
+  while (divContainer.firstChild) {
+    divContainer.firstChild.remove();
+  }
+}
+
+// buttons and their event listeners
+
+reset.addEventListener("click", () => {
+  removeChildNodes();
+});
+
+smllBtn.addEventListener("click", () => {
+  removeChildNodes();
+  createGrid(16);
+});
+
+medBtn.addEventListener("click", () => {
+  removeChildNodes();
+  createGrid(32);
+});
+
+lrgBtn.addEventListener("click", () => {
+  removeChildNodes();
+  createGrid(64);
+});
